39d2a4e76091858230c7713e84cba1b7d2486d2e
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-06-28 21:31:29 +0530

    Update ProductController.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/controllers/ProductController.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/controllers/ProductController.java
@@ -17,3 +17,29 @@ import com.ecommerce.store.services.ProductService;
 import javax.validation.Valid;
 import java.util.List;
 
+@RestController
+@RequestMapping ("/api/v1/products")
+@Api (value = "Product Controller", description = "Available operations on Products")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductController {
+
+    private final ProductService productService;
+
+    @ApiOperation (value = "Create a new Product", response = ResponseEntity.class)
+    @ApiResponses ({
+        @ApiResponse (code = 201, message = "Created"),
+        @ApiResponse (code = 400, message = "Bad Request"),
+        @ApiResponse (code = 403, message = "Forbidden"),
+        @ApiResponse (code = 409, message = "Conflict")
+    })
+    @PostMapping (produces = "application/json")
+    @ResponseBody
+    public ResponseEntity createProduct(@RequestBody @Valid ProductCreationDto product) {
+        log.info("### New Product creation request - {}", product);
+
+        Product created = productService.createProduct(product);
+        return new ResponseEntity(created, HttpStatus.CREATED);
+    }
+
+   
