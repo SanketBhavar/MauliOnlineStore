a52c670ad333de0b7e68c5959c10ddecd679393a
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-06-29 21:06:36 +0530

    Commit in Order file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/Order.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/Order.java
@@ -48,5 +48,25 @@ public class Order implements Serializable {
     @Column (name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Double getTotalPrice() {
+        return items.stream().mapToDouble(item -> item.getUnitPrice() * item.getQuantity()).sum();
+    }
 }
 
