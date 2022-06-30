4a7f6168a932c9bd47a6823d2c30a2128d02dfb5
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-06-30 21:28:19 +0530

    Commit

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/OrderItemPK.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/OrderItemPK.java
@@ -44,6 +44,34 @@ public class OrderItemPK implements Serializable {
 
         return result;
     }
+@Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        OrderItemPK other = (OrderItemPK) obj;
+        if (order == null) {
+            if (other.order != null) {
+                return false;
+            }
+        } else if (!order.equals(other.order)) {
+            return false;
+        }
 
-    
+        if (product == null) {
+            if (other.product != null) {
+                return false;
+            }
+        } else if (!product.equals(other.product)) {
+            return false;
+        }
+
+        return true;
+    }    
 }
