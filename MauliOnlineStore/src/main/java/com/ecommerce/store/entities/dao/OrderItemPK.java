8d17bddd82a8aea298a2c28039cd7123d0443cd1
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-06-30 21:27:22 +0530

    Update OrderItemPK.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/OrderItemPK.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dao/OrderItemPK.java
@@ -12,4 +12,38 @@ import javax.persistence.ManyToOne;
 import java.io.Serializable;
 import static java.util.Objects.nonNull;
 
+@Embeddable
+@Data
+@NoArgsConstructor
+@JsonIdentityInfo (generator = ObjectIdGenerators.PropertyGenerator.class, property = "order")
+public class OrderItemPK implements Serializable {
 
+    @ManyToOne (optional = false, fetch = FetchType.LAZY)
+    @JoinColumn (name = "order_id")
+    private Order order;
+
+    @ManyToOne (optional = false, fetch = FetchType.LAZY)
+    @JoinColumn (name = "product_id")
+    private Product product;
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+
+        result = prime * result + (!nonNull(order.getId())
+                                   ? 0
+                                   : order
+                                       .getId()
+                                       .hashCode());
+        result = prime * result + (!nonNull(product.getId())
+                                   ? 0
+                                   : product
+                                       .getId()
+                                       .hashCode());
+
+        return result;
+    }
+
+    
+}
