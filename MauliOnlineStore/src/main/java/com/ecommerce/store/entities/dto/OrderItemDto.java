2285526e1c422e400d35fd6e7e41f145dae709c7
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-03 21:53:49 +0530

    Update OrderItemDto.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderItemDto.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderItemDto.java
@@ -4,17 +4,3 @@ import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Value;
 
-import javax.validation.constraints.NotNull;
-import java.util.UUID;
-
-@ApiModel (description = "The OrderItem DTO")
-@Value
-@Builder
-public class OrderItemDto {
-
-    @NotNull
-    private UUID productId;
-
-    @NotNull
-    private Integer quantity;
-}
