3367903b92cd8bc5b704cdbedbe465a6efb76e98
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-03 21:52:33 +0530

    Commit in OrderDTO file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderDto.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderDto.java
@@ -3,4 +3,18 @@ package com.ecommerce.store.entities.dto;
 import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Value;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
+@ApiModel (description = "The Order DTO")
+@Value
+@Builder
+public class OrderDto {
+
+    @NotNull
+    List<OrderItemDto> items;
+
+    @NotEmpty
+    private String buyerEmail;
+}
