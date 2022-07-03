d57fa6cb9c0b85429c65b993851d271f3291c150
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-03 21:55:00 +0530

    Commit in Order Item file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderItemDto.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/OrderItemDto.java
@@ -4,3 +4,17 @@ import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@ApiModel (description = "The OrderItem DTO")
+@Value
+@Builder
+public class OrderItemDto {
+
+    @NotNull
+    private UUID productId;
+
+    @NotNull
+    private Integer quantity;
+}
