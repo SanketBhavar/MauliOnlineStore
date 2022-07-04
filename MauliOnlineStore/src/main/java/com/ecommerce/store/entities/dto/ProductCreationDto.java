58d4737b0a3b184a07d6d4c850474c99120047c4
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-04 19:44:28 +0530

    Update ProductCreationDto.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/ProductCreationDto.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/entities/dto/ProductCreationDto.java
@@ -8,17 +8,3 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-@ApiModel (description = "The Product Creation DTO")
-@Value
-@Builder
-public class ProductCreationDto {
-
-    @NotEmpty
-    private String name;
-
-    private String description;
-
-    @NotNull
-    @Positive
-    private Double price;
-}
