e20bc5662e2021994eef1722ed9204ed06ae49f3
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-05 17:25:50 +0530

    Update ProductNotExistsException.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/exceptions/ProductNotExistsException.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/exceptions/ProductNotExistsException.java
@@ -1,8 +1,1 @@
-package com.ecommerce.store.exceptions;
 
-public class ProductNotExistsException extends RuntimeException {
-
-    public ProductNotExistsException(String message) {
-        super(message);
-    }
-}
