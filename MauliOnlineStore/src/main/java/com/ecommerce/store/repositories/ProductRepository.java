ab6a8b819c5c672fe525ff022553398b7457d41f
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-07 20:20:42 +0530

    Update ProductRepository.java

--- MauliOnlineStore/src/main/java/com/ecommerce/store/repositories/ProductRepository.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/repositories/ProductRepository.java
@@ -1,3 +1,8 @@
 package com.ecommerce.store.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ecommerce.store.entities.dao.Product;
+import java.util.UUID;
+
 
 
