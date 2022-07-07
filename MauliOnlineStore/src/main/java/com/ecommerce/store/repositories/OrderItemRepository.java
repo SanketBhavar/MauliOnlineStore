a4c056f798835f9bd88ff47177d48037fb3cab38
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-07 20:18:12 +0530

    Commit in OrderItem file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/repositories/OrderItemRepository.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/repositories/OrderItemRepository.java
@@ -4,3 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ecommerce.store.entities.dao.OrderItem;
 import java.util.UUID;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+}
