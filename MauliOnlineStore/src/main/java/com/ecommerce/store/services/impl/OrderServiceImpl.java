fb2d4334e001cb826e0898c712a6c791a3cc0cfb
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-08 20:53:04 +0530

    Commit in OrderService file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/services/impl/OrderServiceImpl.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/services/impl/OrderServiceImpl.java
@@ -18,3 +18,57 @@ import java.util.List;
 import java.util.Set;
 import static java.util.Objects.isNull;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final ProductRepository productRepository;
+
+    @Override
+    public Order placeOrder(OrderDto order) {
+        checkIfItemsAreEmpty(order);
+
+        Order created = orderRepository.save(Order.builder()
+                                                  .buyerEmail(order.getBuyerEmail())
+                                                  .build());
+        log.info("### New order persisted ###");
+
+        Set<OrderItem> items = new HashSet<>();
+
+        order.getItems().forEach(item -> productRepository
+            .findById(item.getProductId())
+            .map(product -> items.add(orderItemRepository.save(
+                OrderItem.builder()
+                         .order(created)
+                         .product(product)
+                         .quantity(item.getQuantity())
+                         .unitPrice(product.getPrice())
+                         .build()))).orElseThrow(() -> new ProductNotExistsException("One or more of the Products in the request doesn't exist")));
+
+        log.info("### All order items are persisted ###");
+        created.setItems(items);
+
+        return orderRepository.save(created);
+    }
+
+    private void checkIfItemsAreEmpty(OrderDto order) {
+        if (isNull(order) || order.getItems().isEmpty()) {
+            throw new NoItemsInOrderException("Order creation request doesn't contain any Products");
+        }
+    }
+
+    @Override
+    public List<Order> getOrdersByPeriod(LocalDateTime start, LocalDateTime end) {
+        log.info("### Fetching orders from the database ###");
+        return orderRepository.findByCreatedAtBetween(start, end);
+    }
+
+    // ToDo: Add pagination
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+}
