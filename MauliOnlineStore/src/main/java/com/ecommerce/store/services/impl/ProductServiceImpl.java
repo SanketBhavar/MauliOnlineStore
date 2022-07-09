9f0b54a3c164ccf6a3cec25298bdd19ac4927d90
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-09 18:47:30 +0530

    Commit in ProductService file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/services/impl/ProductServiceImpl.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/services/impl/ProductServiceImpl.java
@@ -13,3 +13,47 @@ import java.util.List;
 import static java.util.Objects.nonNull;
 import static org.springframework.util.StringUtils.hasLength;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repository;
+
+    @Override
+    public Product createProduct(ProductCreationDto productDto) {
+        return repository.save(Product.builder()
+                                      .name(productDto.getName())
+                                      .description(productDto.getDescription())
+                                      .price(productDto.getPrice())
+                                      .build());
+    }
+
+    @Override
+    public Product updateProduct(ProductUpdationDto productDto) {
+        return repository
+            .findById(productDto.getId())
+            .map(existingProduct -> {
+                modifyUpdatedFields(productDto, existingProduct);
+                return repository.save(existingProduct);
+                 }
+            ).orElseThrow(() -> new ProductNotExistsException("Product doesn't exist in the system to update"));
+    }
+
+    private void modifyUpdatedFields(ProductUpdationDto productDto, Product existingProduct) {
+        if (hasLength(productDto.getName())) {
+            existingProduct.setName(productDto.getName());
+        }
+        if (hasLength(productDto.getDescription())) {
+            existingProduct.setDescription(productDto.getDescription());
+        }
+        if (nonNull(productDto.getPrice())) {
+            existingProduct.setPrice(productDto.getPrice());
+        }
+    }
+    // ToDo: Add pagination
+    @Override
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+}
