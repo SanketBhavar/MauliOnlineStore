bd3a311696c9d82925d6884745fa4b570db93e42
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-11 17:34:43 +0530

    Commit in ProductService file

--- MauliOnlineStore/src/main/java/com/ecommerce/store/services/ProductService.java
+++ MauliOnlineStore/src/main/java/com/ecommerce/store/services/ProductService.java
@@ -3,6 +3,10 @@ package com.ecommerce.store.services;
 import com.ecommerce.store.entities.dao.Product;
 import com.ecommerce.store.entities.dto.ProductCreationDto;
 import com.ecommerce.store.entities.dto.ProductUpdationDto;
-
 import java.util.List;
 
+public interface ProductService {
+    Product createProduct(ProductCreationDto productDto);
+    Product updateProduct(ProductUpdationDto productDto);
+    List<Product> getAllProducts();
+}
